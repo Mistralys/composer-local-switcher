17b5e4de10f15e99ac6c8a0642a12b3e7c23ca6e
Mistralys <s.mordziol@gmail.com>
2025-09-09 17:50:20 +0200

    Added `getContent()` in LockFile.

--- src/Utils/LockFile.php
+++ src/Utils/LockFile.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Mistralys\ComposerSwitcher\Utils;
 
+use Mistralys\ComposerSwitcher\ComposerSwitcherException;
+
 class LockFile extends BaseFile
 {
     /**
@@ -16,6 +18,20 @@ class LockFile extends BaseFile
         $this->configFile = $configFile;
 
         parent::__construct(str_replace('.json', '.lock', $configFile->getPath()));
+    }
+
+    public function getContent() : string
+    {
+        $content = file_get_contents($this->getPath());
+
+        if($content !== false) {
+            return $content;
+        }
+
+        throw new ComposerSwitcherException(
+            'Failed to read file: ' . $this->getPath(),
+            ComposerSwitcherException::ERROR_CANNOT_READ_FILE
+        );
     }
 
     public function getConfigFile(): ConfigFile
